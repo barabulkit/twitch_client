520e5b53e7b34a8aa8b8bb2c99a63142a6682a3f
EleshNorn <densolo777@gmail.com>
2020-11-09 06:29:27 +0300

    empty window

--- src/ui/ui.go
+++ src/ui/ui.go
@@ -1,9 +1,25 @@
 package ui
 
 import (
+	"log"
 	"github.com/gotk3/gotk3/gtk"
 )
 
-func render() {
+func Render() {
 	gtk.Init(nil)
+
+	win, err := gtk.WindowNew(gtk.WINDOW_TOPLEVEL)
+	if err != nil {
+		log.Fatal("Unable to create window", err)
+	}
+
+	win.SetTitle("Twitch client")
+	win.Connect("destroy", func() {
+		gtk.MainQuit()
+	})
+
+	win.SetDefaultSize(800, 600)
+	win.ShowAll()
+
+	gtk.Main()
 }
